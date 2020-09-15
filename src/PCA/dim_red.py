5eee39f8fb880b61d91f0751b09bec76e864d4b8
mapl <mapl@itu.dk>
2020-09-15 21:12:31 +0200

    not working code for PCA - weird issue to look at. Preprocessing done. To be figured out why the error comes up.

---
+++ src/PCA/dim_red.py
@@ -0,0 +1,81 @@
+import numpy as np
+from scipy.linalg import svd
+
+class PCA():
+    # different versions of datasets used in different stages
+    raw_dataset, adj_dataset, pca_ready_dataset = [], [], []
+    cat_cols_idx = []#[1,3,4,5,6,7,8,9] #columns that need to be 1-of-K encoded
+    lookup_dict = {}
+
+
+    def __init__(self, filename):
+        self.read_data(filename)
+        self.prep_dataset()
+        self.sub_mean()
+        self.apply_PCA()
+
+    def read_data(self, filename):
+        # simply loads the dataset into raw_dataset
+        with open(filename) as infile:
+            next(infile)
+            for line in infile:
+                line = line.strip().split(",")
+                line = list(map(float, line))
+                self.raw_dataset.append(line)
+        self.raw_dataset = np.array(self.raw_dataset)
+
+    def prep_dataset(self):
+        # prepare the dataset - a bit convoluted for allowing 1-of-K encoding.
+        # ignore for now
+        for col in range(self.raw_dataset.shape[1]):
+            if col in self.cat_cols_idx:
+                old_col = self.raw_dataset[:,col]
+                classes = int(np.max(old_col))
+                new_col = np.zeros((old_col.size, classes))
+                for i, value in enumerate(old_col):
+                    new_col[i][int(value)-1] = 1
+
+                self.lookup_dict[col] = new_col
+            else:
+                self.lookup_dict[col] = np.mean(self.raw_dataset[:,col])
+
+        
+        for row in range(self.raw_dataset.shape[0]):
+            row_vector = []
+            for col in range(self.raw_dataset.shape[1]):
+                if col in self.cat_cols_idx:
+                    row_vector.append(self.lookup_dict[col][row])
+                else:
+                    row_vector.append(self.raw_dataset[row][col])
+                   
+            row_vector = np.array(row_vector, dtype='object')
+            self.adj_dataset.append(row_vector)
+        self.adj_dataset = np.array(self.adj_dataset)
+
+    def sub_mean(self):
+        # for columns that are not 1-of-K encoded we subtract the mean
+        for col in range(self.adj_dataset.shape[1]):
+            if col not in self.cat_cols_idx:
+                self.lookup_dict[col] = [self.lookup_dict[col]]*self.adj_dataset.shape[0]
+                self.pca_ready_dataset.append(self.adj_dataset[:,col] - self.lookup_dict[col])
+            else:
+                self.pca_ready_dataset.append(self.adj_dataset[:,col])
+        self.pca_ready_dataset = np.array(self.pca_ready_dataset).T
+
+    def apply_PCA(self):
+        # this is complaining. Not sure why. I am inputting a np array (1385,29) just like ex2_1_5.py
+        # but this returns errors. Need to fix this.
+        print(self.pca_ready_dataset[0:5])
+        print(type(self.pca_ready_dataset))
+        print(type(self.pca_ready_dataset[0]))
+        print(type(self.pca_ready_dataset[0][1]))
+        print(self.pca_ready_dataset.shape)
+        U,S,Vh = svd(self.pca_ready_dataset, full_matrices=False)
+        V = Vh.T
+
+    
+
+
+if __name__ == "__main__":
+    pca = PCA("src/data/HCV-Egy-Data.csv")
+    
