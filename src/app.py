4c7e88ac8a049e18b13b128af600d2aa5ec68afa
Jesper Vang <jvang@jespers-mbp.lan>
2020-10-02 13:26:46 +0200

    Several things 🤞

--- src/app.py
+++ src/app.py
@@ -1,151 +1,131 @@
-############################################################
-#  Synode´s implimentation of save state				                                        #
-# https://gist.github.com/Ghasel/0aba4869ba6fdc8d49132e6974e2e662          #
-############################################################
+#!/usr/bin/env python3
+ 
+##########################################################
+# encoding: utf                                          #
+# Copyright (c) Jesper Vang <jesper_vang@me.com>         #
+# MIT Licence. See http://opensource.org/licenses/MIT    #
+# Created on 11 Sep 2020                                 #
+# Version:	0.0.1                                        #
+##########################################################
 
+import pandas as pd
 import streamlit as st
-from streamlit.hashing import _CodeHasher
+import plotly.express as px
+import SessionState
+from pathlib import Path
+from report1 import display_reports
+import base64
 
-try:
-    # Before Streamlit 0.65
-    from streamlit.ReportThread import get_report_ctx
-    from streamlit.server.Server import Server
-except ModuleNotFoundError:
-    # After Streamlit 0.65
-    from streamlit.report_thread import get_report_ctx
-    from streamlit.server.server import Server
+
+ABOUT = "About"
+REPORTS = "Reports"
+DATA = "Data"
+CONFIG = "Config"
 
 
 def main():
-    state = _get_state()
-    pages = {
-        "Dashboard": page_dashboard,
-        "Settings": page_settings,
+    PAGES = {
+        ABOUT: display_about,
+        REPORTS: display_reports,
+        DATA: dispaly_data,
+        CONFIG: display_config,
+        
     }
 
-    st.sidebar.title(":floppy_disk: Page states")
-    page = st.sidebar.radio("Select your page", tuple(pages.keys()))
+    st.sidebar.header("Hepatitis")
+    page = st.sidebar.radio("Select page", options=list(PAGES.keys()))
+    display_sidebar_settings()
 
-    # Display the selected page with the session state
-    pages[page](state)
+    PAGES[page]()
 
-    # Mandatory to avoid rollbacks with widgets, must be called at the end of your app
-    state.sync()
+def display_sidebar_settings():
+    st.sidebar.markdown("---")
+    st.sidebar.markdown("ℹ️ ** Details **")
+    desc_check = st.sidebar.checkbox("📃 Dataset Description")
+    desc_markdown = read_markdown_file("src/desc_markdown.md")        
 
-
-def page_dashboard(state):
-    st.title(":chart_with_upwards_trend: Dashboard page")
-    display_state_values(state)
-
-
-def page_settings(state):
-    st.title(":wrench: Settings")
-    display_state_values(state)
-
-    st.write("---")
-    options = ["Hello", "World", "Goodbye"]
-    state.input = st.text_input("Set input value.", state.input or "")
-    state.slider = st.slider("Set slider value.", 1, 10, state.slider)
-    state.radio = st.radio("Set radio value.", options, options.index(state.radio) if state.radio else 0)
-    state.checkbox = st.checkbox("Set checkbox value.", state.checkbox)
-    state.selectbox = st.selectbox("Select value.", options, options.index(state.selectbox) if state.selectbox else 0)
-    state.multiselect = st.multiselect("Select value(s).", options, state.multiselect)
-
-    # Dynamic state assignments
-    for i in range(3):
-        key = f"State value {i}"
-        state[key] = st.slider(f"Set value {i}", 1, 10, state[key])
+    if desc_check:
+        st.sidebar.markdown(desc_markdown, unsafe_allow_html=True)
 
 
-def display_state_values(state):
-    st.write("Input state:", state.input)
-    st.write("Slider state:", state.slider)
-    st.write("Radio state:", state.radio)
-    st.write("Checkbox state:", state.checkbox)
-    st.write("Selectbox state:", state.selectbox)
-    st.write("Multiselect state:", state.multiselect)
-    
-    for i in range(3):
-        st.write(f"Value {i}:", state[f"State value {i}"])
-
-    if st.button("Clear state"):
-        state.clear()
 
 
-class _SessionState:
+@st.cache
+def load_data():
+    return pd.read_csv(
+        "src/data/HCV-Egy-Data.csv"
+    ).reset_index(drop=True)
 
-    def __init__(self, session, hash_funcs):
-        """Initialize SessionState instance."""
-        self.__dict__["_state"] = {
-            "data": {},
-            "hash": None,
-            "hasher": _CodeHasher(hash_funcs),
-            "is_rerun": False,
-            "session": session,
-        }
+@st.cache
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
 
-    def __call__(self, **kwargs):
-        """Initialize state data once."""
-        for item, value in kwargs.items():
-            if item not in self._state["data"]:
-                self._state["data"][item] = value
+def display_config():
+    pass
 
-    def __getitem__(self, item):
-        """Return a saved state value, None if item is undefined."""
-        return self._state["data"].get(item, None)
-        
-    def __getattr__(self, item):
-        """Return a saved state value, None if item is undefined."""
-        return self._state["data"].get(item, None)
-
-    def __setitem__(self, item, value):
-        """Set state value."""
-        self._state["data"][item] = value
-
-    def __setattr__(self, item, value):
-        """Set state value."""
-        self._state["data"][item] = value
-    
-    def clear(self):
-        """Clear session state and request a rerun."""
-        self._state["data"].clear()
-        self._state["session"].request_rerun()
-    
-    def sync(self):
-        """Rerun the app with all state values up to date from the beginning to fix rollbacks."""
-
-        # Ensure to rerun only once to avoid infinite loops
-        # caused by a constantly changing state value at each run.
-        #
-        # Example: state.value += 1
-        if self._state["is_rerun"]:
-            self._state["is_rerun"] = False
-        
-        elif self._state["hash"] is not None:
-            if self._state["hash"] != self._state["hasher"].to_bytes(self._state["data"], None):
-                self._state["is_rerun"] = True
-                self._state["session"].request_rerun()
-
-        self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
+#def display_reports():
+    return display_reports
 
 
-def _get_session():
-    session_id = get_report_ctx().session_id
-    session_info = Server.get_current()._get_session_info(session_id)
+def display_about():
+    intro_markdown = read_markdown_file("src/intro.md")
+    st.markdown(intro_markdown, unsafe_allow_html=True)
 
-    if session_info is None:
-        raise RuntimeError("Couldn't get your Streamlit Session object.")
-    
-    return session_info.session
+def dispaly_data():
+     # Header
+    st.title("Data exploration")
+    # For univariate distributions
+    # histogram to better understand
+    st.header("Histogram")
+    hist_x = st.selectbox("Histogram variable", options=df.columns, index=df.columns.get_loc("Gender"))
+    hist_bins = st.slider(label="Histogram bins", min_value=5, max_value=50, value=25, step=1)
+    hist_cats = df[hist_x].sort_values().unique()
+    hist_fig = px.histogram(df, x=hist_x, nbins=hist_bins, title="Histogram of " + hist_x,
+                            template="plotly_white", category_orders={hist_x: hist_cats})
+    st.write(hist_fig)
 
 
-def _get_state(hash_funcs=None):
-    session = _get_session()
 
-    if not hasattr(session, "_custom_session_state"):
-        session._custom_session_state = _SessionState(session, hash_funcs)
+    # boxplots
+    st.header("Boxplot")
+    st.subheader("With a categorical variable - Fever, Gender or BMI")
+    box_x = st.selectbox("Boxplot variable", options=df.columns, index=df.columns.get_loc("WBC"))
+    box_cat = st.selectbox("Categorical variable", ["Fever", "Gender", "BMI"], 0)
+    box_fig = px.box(df, x=box_cat, y=box_x, title="Box plot of " + box_x,
+                            template="plotly_white", category_orders={"pos_simple": ["PG", "SG", "SF", "PF", "C"]})
+    st.write(box_fig)
 
-    return session._custom_session_state
+    # # min filter
+    # st.header("Correlations")
+    # corr_x = st.selectbox("Correlation - X variable", options=df.columns, index=df.columns.get_loc("Gender"))
+    # corr_y = st.selectbox("Correlation - Y variable", options=df.columns, index=df.columns.get_loc("WBC"))
+    # corr_col = st.radio("Correlation - color variable", options=["Gender", "Fever", "Headache"], index=1)
+    # corr_filt = st.selectbox("Filter variable", options=df.columns, index=df.columns.get_loc("WBC"))
+    # min_filt = st.number_input("Minimum value", value=6, min_value=0)
+    # tmp_df = df[df[corr_filt] > min_filt]
+    # fig = px.scatter(tmp_df, x=corr_x, y=corr_y, template="plotly_white", render_mode='webgl',
+    #                  color=corr_col, hover_data=['WBC', 'RBC', 'Age', 'Gender'], color_continuous_scale=px.colors.sequential.OrRd,
+    #                  category_orders={"pos_simple": ["PG", "SG", "SF", "PF", "C"]})
+    # fig.update_traces(mode="markers", marker={"line": {"width": 0.4, "color": "slategrey"}})
+    # st.subheader("Filtered scatterplot and dataframe")
+    # st.write(fig)
+    # st.write(tmp_df)
+
+    # correlation heatmap
+    hmap_params = st.multiselect("Select parameters to include on heatmap", options=list(df.columns), default=list(df.columns))
+    hmap_fig = px.imshow(df[hmap_params].corr())
+    st.write(hmap_fig)
+
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    return base64.b64encode(img_bytes).decode()
+
+header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+    img_to_bytes("src/image/header.png")
+)
+st.markdown(
+    header_html, unsafe_allow_html=True,
+)
 
 
 if __name__ == "__main__":
