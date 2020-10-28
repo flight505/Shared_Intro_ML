0e5c9e3cd167efde701c4af93165085c10e934b1
mapl <mapl@itu.dk>
2020-10-28 10:20:58 +0100

    added api example

--- src/models/example.py
+++ src/models/example.py
@@ -1,2 +1,10 @@
 from loader import reader
-print(reader().get_all('classification'))
+
+task = 'classification'
+x_train, y_train, x_test, y_test = reader().get_all(task)
+
+x_train = reader().get_x_train(task)
+y_train = reader().get_y_train(task)
+x_test = reader().get_x_test(task)
+y_test = reader().get_y_test(task)
+
